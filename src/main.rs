d15a71c3bc7fa1d5627bc820abb87fabf3bcb817
benjo121ben <benjamin.wenger00@gmail.com>
2025-05-21 01:12:35 +0200

    added config read

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ async fn main() {
     use leptos_axum::{generate_route_list, LeptosRoutes};
     use test_leptos::app::*;
 
-    let conf = get_configuration(None).unwrap();
+    let conf = get_configuration(Some("Cargo.toml")).unwrap();
     let addr = conf.leptos_options.site_addr;
     let leptos_options = conf.leptos_options;
     // Generate the list of routes in your Leptos App
